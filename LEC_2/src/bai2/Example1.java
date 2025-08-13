d4f3fe6ad94c089414e5808e7671723a8bd513e1
hungmai1106 <maihung1106@gmail.com>
2025-08-13 20:14:23 +0700

    Add files via upload

---
+++ LEC_2/src/bai2/Example1.java
@@ -0,0 +1,13 @@
+
+package bai2;
+
+import java.awt.BorderLayout;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int x = 1;
+        int z = x - x-- - x++ + --x - x + x-- + x;
+        System.out.println("z= " + z);
+        
+    }
+}
